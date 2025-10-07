4da17aed85acca6693ca4b2097bc766f6c68192f
Malo Bourry <malo.bourry@theodo.com>
2025-10-07 14:37:16 +0200

    fix : gestion ruff 2

--- src/scripts/simulate_sensor_data.py
+++ src/scripts/simulate_sensor_data.py
@@ -4,17 +4,13 @@ import time
 
 import requests
 
-# URL de destination
 url = "https://:5BLXGdo4TSiXZfaB33y1QQ@eu.tandem.autodesk.com/api/v1/timeseries/models/urn:adsk.dtm:RcE1IDqYSLGLpmtoVha9Rg/streams/AQAAACJBLUpcr0ywnmQzrZqKLmwAAAAA"
 
-# Envoi toutes les 100 ms (~10 fois par seconde)
 interval = 10
 
 
-# Fonction pour générer une valeur oscillante entre 20 et 25
 def oscillating_value(t):
     """Retourne une valeur oscillante entre 20 et 25 basée sur le temps t."""
-    # Sinusoïde entre 20 et 25
     return 22.5 + 2.5 * math.sin(2 * math.pi * t / 5)
 
 
@@ -37,9 +33,9 @@ try:
             if response.status_code != 200:
                 print(f"Erreur HTTP {response.status_code}: {response.text}")
         except requests.RequestException as e:
-            print(f"Erreur d’envoi : {e}")
+            print(f"Erreur d envoi : {e}")
 
         time.sleep(interval)
 
 except KeyboardInterrupt:
-    print("\nArrêt de l’envoi des données.")
+    print("\nArrêt de l envoi des données.")
