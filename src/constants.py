0e41797f9ecc4d85edbf9035bb70474248fc8a45
Malo Bourry <malo.bourry@theodo.com>
2025-10-13 10:15:58 +0200

    ajout des identifiants du projet GCP

--- src/constants.py
+++ src/constants.py
@@ -4,11 +4,19 @@ import os
 from pathlib import Path
 
 PROJECT_ROOT_PATH = Path(__file__).parents[1]
+
+# Tandem
 URL_CAR_SENSOR = os.environ.get("URL_CAR_SENSOR")
 URL_TEMPERATURE_SENSOR = os.environ.get("URL_TEMPERATURE_SENSOR")
+TANDEM_VEHICLE_COUNT_PARAMETER_NAME = "count_vehicle_value"
 
+# YOLO
 CONF_THRESH = 0.3
 TARGET_CLASSES = ["mini-car"]
 MIN_FRAMES_VISIBLE = 10
 MIN_MISSING_FRAMES = 10
-TANDEM_VEHICLE_COUNT_PARAMETER_NAME = "count_vehicle_value"
+
+
+# GCP Pub/Sub
+PROJECT_ID = "ramery-poc-theodo"
+TOPIC_ID = "sensor-topic"
